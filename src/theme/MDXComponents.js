eb05e657c68a6e6e82020e6277643bde9465d3d4
dsmtE <desmet.enguerrand@gmail.com>
2022-09-07 01:55:31 +0200

    ✨ VSCode extension component

---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MDXComponents from '@theme-original/MDXComponents';
+import VSCodeExtension from '@site/src/components/VSCodeExtension';
+
+export default {
+  ...MDXComponents,
+  VSCodeExtension: VSCodeExtension,
+};
